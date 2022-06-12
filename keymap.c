e8d9cd67e40ccf7bc7e929723688d4d59c0fa124
Yuki Yoshimine <yuki07yuki@gmail.com>
2022-06-12 18:51:42 +0800

    Remove FANCY_KEY

--- keymap.c
+++ keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
   KC_ESC,   KC_A,   KC_S,     KC_D,   KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
   SFT_UNDERSCORE,  KC_Z,  KC_X,    KC_C,  KC_V,  KC_B, KC_MUTE,     XXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-    KC_LGUI, MO(_TMUX), LEFT_THUMB, MT(MOD_LCTL, KC_ENT), OSL(_SYMBOL),      TD(CAPSWORD),  MT(MOD_RCTL, KC_SPC), FANCY_KEY, KC_RALT, KC_RGUI
+    KC_LGUI, MO(_TMUX), MT(MOD_LCTL, KC_ENT), LEFT_THUMB, OSL(_SYMBOL),      TD(CAPSWORD),  MT(MOD_RCTL, KC_SPC), MO(_SYMBOL), KC_RALT, KC_RGUI
 ),
 
 

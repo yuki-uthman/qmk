702f8d0569418d643af5973f8c4114ca673e450e
Yuki Yoshimine <yuki07yuki@gmail.com>
2022-08-09 10:56:16 +0800

    Add custom c and v for copy and paste

--- keymap.c
+++ keymap.c
@@ -18,6 +18,9 @@
 #define RSHIFT LT(0, KC_6)
 #define LSHIFT LT(0, KC_5)
 
+#define CUSTOM_C LT(0, KC_C)
+#define CUSTOM_V LT(0, KC_V)
+
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
@@ -99,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSLS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   MAC,   KC_A,   KC_S,     KC_D,   KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-  ___,  KC_Z,  KC_X,    KC_C,  KC_V,  KC_B, KC_MUTE,     XXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+  ___,  KC_Z,  KC_X,    CUSTOM_C,  CUSTOM_V,  KC_B, KC_MUTE,     XXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
     MO(_TMUX), KC_LALT, ___, LEFT_THUMB, OSM(MOD_LSFT),       OSL(_SYMBOL),  RIGHT_THUMB, ___, KC_RALT, KC_RGUI
 ),
 
@@ -136,6 +139,18 @@ ___,   KC_CIRC, KC_SLSH,  KC_ASTR,  KC_BSLS,  ___,  ___,   ___, ___, ___,   KC_L
 ),
 
 };
+
+/* https://getreuer.info/posts/keyboards/triggers/index.html#tap-vs.-long-press */
+static bool process_tap_or_long_press_key(
+    keyrecord_t* record, uint16_t long_press_keycode) {
+  if (record->tap.count == 0) {  // Key is being held.
+    if (record->event.pressed) {
+      tap_code16(long_press_keycode);
+    }
+    return false;  // Skip default handling.
+  }
+  return true;  // Continue default handling.
+}
 
 /* void matrix_scan_user(void) { */
 /*     clear_recent_keys();  // Timed out; clear the buffer. */
@@ -209,21 +224,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 set_single_persistent_default_layer(_QWERTY);
             }
             return false;
-        case LSHIFT:
-            if (record->tap.count > 0) {    // Key is being tapped.
-                if (record->event.pressed) {
-                    xprintf("left shift pressed");
-                    register_code16(KC_CAPS);
-                } else {
-                    xprintf("left shift released");
-                    unregister_code16(KC_CAPS);
-                }
-            } else {                        // Key is being held.
-                if (record->event.pressed) {
-                } else {
-                }
-            }
-            return false;
+
+        case CUSTOM_C:  // Comma on tap, Ctrl+C on long press.
+          return process_tap_or_long_press_key(record, G(KC_C));
+
+        case CUSTOM_V:  // Dot on tap, Ctrl+V on long press.
+          return process_tap_or_long_press_key(record, G(KC_V));
+
         case LEFT_THUMB:
             if (record->tap.count > 0) {    // Key is being tapped.
                 if (record->event.pressed) {

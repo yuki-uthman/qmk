c3bb91a5344af368fa45b3d98657dd15467f5cb0
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-02-02 08:32:17 +0800

    change: replace left number symbol to CAPSWORD

--- keymap.c
+++ keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   MAC,   KC_A,   KC_S,     KC_D,   KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
   ___,  KC_Z,  KC_X,    CUSTOM_C,  CUSTOM_V,  KC_B, KC_MUTE,     XXX, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-    MO(_TMUX), KC_LALT, OSL(_NUMBER), LEFT_THUMB, OSL(_SYMBOL),       OSL(_SYMBOL),  RIGHT_THUMB, CAPSWORD, KC_RALT, KC_RGUI
+    MO(_TMUX), KC_LALT, CAPSWORD, LEFT_THUMB, OSL(_SYMBOL),       OSL(_SYMBOL),  RIGHT_THUMB, CAPSWORD, KC_RALT, KC_RGUI
 ),
 
 

09a528b29f5ff00b59d28d04fcc59bf5d2e182a1
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-02-02 12:13:19 +0800

    fix: add CUSTOM_C CUSTOM_V keys to capsword
    without the case statement, c and v cannot be capitalized

--- keymap.c
+++ keymap.c
@@ -125,6 +125,8 @@ bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
         // Keycodes that continue Caps Word, with shift applied.
         case KC_A ... KC_Z:
+        case 34:
+        case 35:
         case KC_MINS:
           add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to the next key.
           return true;

d73c85388bb08fe321b264b79a97cbfa6ccd7d6e
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-06-09 00:30:32 +0800

    main: remove tab

--- keymap.c
+++ keymap.c
@@ -40,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      XXX,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_BSLS,
-  KC_ESC,   KC_A,   KC_S,     KC_D,   KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_QUOT,
+  XXX,      KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      XXX,
+  KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_BSLS,
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_QUOT,
   ___,  KC_Z,  KC_X,    CUSTOM_C,  CUSTOM_V,  KC_B, KC_MUTE,        XXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUES,   KC_EXLM,
     TMUX, KC_LALT, CAPSWORD, LEFT_THUMB, OSL(_SYMBOL),       OSL(_SYMBOL),  RIGHT_THUMB, CAPSWORD, KC_RALT, TMUX
 ),

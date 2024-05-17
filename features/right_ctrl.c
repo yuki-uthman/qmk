22f8ad23abb26d3fde92f3b7c1d526e4e505f3db
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-05-17 06:31:21 +0500

    right_thumb: remove unused keys

--- features/right_ctrl.c
+++ features/right_ctrl.c
@@ -33,93 +33,10 @@ bool process_right_ctrl(uint16_t keycode, const keyrecord_t *record) {
 
     switch (keycode) {
 
-    // =
-    case KC_J:
-        if (record->event.pressed) {
-            register_code16(KC_EQL);
-        } else {
-            unregister_code16(KC_EQL);
-        }
-        return false;
-
-    // backslash
-    case KC_M:
-        if (record->event.pressed) {
-            register_code16(KC_BSLS);
-        } else {
-            unregister_code16(KC_BSLS);
-        }
-        return false;
-
-    // "
-    case KC_QUOT:
-        if (record->event.pressed) {
-            register_code16(KC_DQUO);
-        } else {
-            unregister_code16(KC_DQUO);
-        }
-        return false;
-
-    // []
-    case KC_8:
-        if (record->event.pressed) {
-            register_code16(KC_LBRC);
-        } else {
-            unregister_code16(KC_LBRC);
-        }
-        return false;
-    case KC_9:
-        if (record->event.pressed) {
-            register_code16(KC_RBRC);
-        } else {
-            unregister_code16(KC_RBRC);
-        }
-        return false;
-
-    // {}
-    case KC_O:
-        if (record->event.pressed) {
-            register_code16(KC_RCBR);
-        } else {
-            unregister_code16(KC_RCBR);
-        }
-        return false;
-    case KC_I:
-        if (record->event.pressed) {
-            register_code16(KC_LCBR);
-        } else {
-            unregister_code16(KC_LCBR);
-        }
-        return false;
-
-    // ()
-    case KC_L:
-        if (record->event.pressed) {
-            register_code16(KC_RPRN);
-        } else {
-            unregister_code16(KC_RPRN);
-        }
-        return false;
     case KC_K:
         if (record->event.pressed) {
             tap_code16(S(KC_Z));
             tap_code16(S(KC_Z));
-        }
-        return false;
-
-    // <>
-    case KC_COMM:
-        if (record->event.pressed) {
-            register_code16(KC_LABK);
-        } else {
-            unregister_code16(KC_LABK);
-        }
-        return false;
-    case KC_DOT:
-        if (record->event.pressed) {
-            register_code16(KC_RABK);
-        } else {
-            unregister_code16(KC_RABK);
         }
         return false;
 
